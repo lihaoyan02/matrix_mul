b025a2e00be7dd2802687843187953f86011d90a
lihaoyan02 <1620774239@qq.com>
2025-09-27 16:52:28 +0800

    modified README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # matrix_mul
+## Completed: 
+Systolic Array Matrix multiplier without pipeline (matrix_mul.v)
+Booth's multiplication algorithm with Dadda tree (one clock cycle) (mul_sign_int.v)
+## To be complete:
+pipelined Matrix multiplier
+Booth's multiplication algorithm in multi-clock-cycle (area saving) (mul_mc.v)
--- new/mul_mc.v
+++ new/mul_mc.v
@@ -1,49 +1,56 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 
-// Design Name: 
-// Module Name: MULINT
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
+// Project Description: multiply for multi-cycle
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
 `define BOOTHCODE_LEN 3
 `define PART_PRDT_NUM 5
+`define CNT_MAX 5
 
 module mul_mc
 #(
 parameter DATA_W=8
 )
 (
-        // input       clk,
-        // input       rst_n,
+        input       clk,
+        input       rst_n,
+        input       in_valid,
+        output      in_ready,
         input       [DATA_W-1:0]    op1,
         input       [DATA_W-1:0]    op2,
         
-        output      [2*DATA_W-1:0]    dout_C
+        output      [2*DATA_W-1:0]    dout_C,
+        output      out_valid,
+        input       out_ready
         
     );
     
-    wire [32:0] part_prdt_hi_r;
-    wire [32:0] part_prdt_lo_r;
+    reg [32:0] part_prdt_hi_r;
+    reg [32:0] part_prdt_lo_r;
+    wire op1_sign = op1[DATA_W-1];
+    wire op2_sign = op2[DATA_W-1];
 
     wire [2:0] booth_code [4:0];
     //扩展位用于编码，左边补两位符号位，右边补一位0
-    wire op2_expd = {2{op2[DATA_W-1]},op2,0}
-    
+    wire op2_expd = {op2_sign,op2_sign,op2,1'b0};
+
+    wire sel = in_valid & in_ready;
+    // 计数器
+    reg [2:0] cnt;
+    always @(posedge clk or negedge rst_n) begin
+        if(!rst_n)
+            cnt <= 3'd0;
+        else if(sel)
+            cnt <= 3'd1;
+        else if (cnt>=`CNT_MAX)
+            cnt <= 3'd0;
+        else if (cnt>0)
+        cnt <= cnt + 3'd1;
+    end
+
     //对应编码位置进行连接
+    // to be edited
     genvar i;
     generate
         for ( i=0 ;i<5 ;i=i+1 ) begin
